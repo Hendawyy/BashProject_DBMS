6fc1eab948bf2d71666bf24c1a81cd117f98ab8a
Ahmed Amin IV <AhmedAminSamey@gmail.com>
2023-08-01 22:20:59 +0300

    decalred the function before calling it

--- Create_DB.sh
+++ Create_DB.sh
@@ -5,6 +5,23 @@
 	later on we should use awk to get the reset of the input from the gui
 	for now database name are case senstive
 '
+function db_name_check {
+x=$db_name
+#x=$1
+echo $x
+if [[ $x =~ [\'\"\^\#\`\~\$\%\=\+\<\>\|\:\ \(\)\@\;\?\&\*\\\/]+ ]]
+then
+        echo "invalid name, avoid using special character"
+        echo "like: ws, &, *, @"
+elif [[ $x =~ ^[0-9] ]]
+then
+        echo "DB name can't start with numbers"
+else
+        echo "valid name"
+        mkdir $full_path
+fi
+}
+
 read -p "enter DB name: " db_name #can be replaced with $1 if we have argument
 full_path="$PWD/$db_name"
 if [[ -d $full_path ]]; then			#will return true if DB exists
@@ -12,22 +29,3 @@ if [[ -d $full_path ]]; then			#will return true if DB exists
 else						#DB doesn't exist
 	 db_name_check
 fi
-
-
-function db_name_check {
-x=$db_name
-#x=$1
-echo $x
-if [[ $x =~ [\'\"\^\#\`\~\$\%\=\+\<\>\|\:\ \(\)\@\;\?\&\*\\\/]+ ]]
-then
-	echo "invalid name, avoid using special character"
-	echo "like: ws, &, *, @"
-elif [[ $x =~ ^[0-9] ]]
-then
-	echo "DB name can't start with numbers"
-else
-	echo "valid name"
-	mkdir $full_path
-fi
-}
-
