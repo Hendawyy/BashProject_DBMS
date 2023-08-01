3bd17f7ea2250b6605c09da0264d62a69be72ca5
Ahmed Amin IV <AhmedAminSamey@gmail.com>
2023-08-01 21:06:46 +0300

    added a function to check name constraint

--- Create_DB.sh
+++ Create_DB.sh
@@ -7,12 +7,27 @@
 '
 read -p "enter DB name: " db_name #can be replaced with $1 if we have argument
 full_path="$PWD/$db_name"
-if [ -d $full_path ]; then			#will return true if DB exists
+if [[ -d $full_path ]]; then			#will return true if DB exists
   echo "database already exists, try another name."
 else						#DB doesn't exist
-       : 'we should implement function for regex and DB names constrains
-	 
-	 it should be used before creating tables too
-	 '
-	 #mkdir $full_path
+	 db_name_check
 fi
+
+
+function db_name_check {
+x=$db_name
+#x=$1
+echo $x
+if [[ $x =~ [\'\"\^\#\`\~\$\%\=\+\<\>\|\:\ \(\)\@\;\?\&\*\\\/]+ ]]
+then
+	echo "invalid name, avoid using special character"
+	echo "like: ws, &, *, @"
+elif [[ $x =~ ^[0-9] ]]
+then
+	echo "DB name can't start with numbers"
+else
+	echo "valid name"
+	mkdir $full_path
+fi
+}
+
