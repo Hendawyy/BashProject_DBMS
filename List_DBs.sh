34fe7515dc39cdfcb7e6ef80c8ddfb17f873e712
Seif Hendawy <seifhendawy1@gmail.com>
2023-08-02 19:03:59 +0300

    update the dirs

--- List_DBs.sh
+++ List_DBs.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-#desc: list all databases in vertical order
-
-ls -d /Databases/ */ --format=single-column | sed 's/\//.db/g'
+ls -d Databases/* | sed 's|.*/||' | awk '{print $0 ".db"}'
