dcc435737057a15ce51bca62c182ce446193c670
Seif Hendawy <108102491+Hendawyy@users.noreply.github.com>
2023-08-03 01:28:19 +0300

    Update Create_DB.sh

--- Create_DB.sh
+++ Create_DB.sh
@@ -27,41 +27,6 @@ function create_database() {
   done
 }
 
-create_database
- 
-=======
-
-=======
-
-
-source components.sh
-
-function create_database() {
-  while true; do
-    db_name=$(zenity --entry \
-      --title="Create Database" \
-      --text="Enter The DB Name:")
-
-    if [ $? -eq 1 ]; then
-      DBmenu
-      return
-    fi
-
-    if ! chckNameRegex "$db_name"; then
-      continue
-    fi
-    if [ -d "Databases/$db_name" ]; then
-        zenity --error --text="A database with the name '$db_name' already exists."
-     else
-        mkdir -p "Databases/$db_name"
-        zenity --info --text="Database '$db_name'.db created successfully!"
-        return
-
-    fi
-  done
-}
-
-create_database
 
 # fun_name: create_data_base
 : 'desc: a script to check if a directory (database) exists if not it creates the database, else it return "data base already exist try another name" 
