e2d09c1a1f2a97892371e08d1800a09888daa5f2
Seif Hendawy <108102491+Hendawyy@users.noreply.github.com>
2023-08-03 01:27:30 +0300

    Update components Add List functions.sh

--- components.sh
+++ components.sh
@@ -41,3 +41,38 @@ function check_if_dir_exists {
 		echo false
 	fi
 }
+
+
+
+function list_databases() {
+  local database_list=$(ls -d Databases/* | sed 's|.*/||' | awk '{print $0 ".db"}')
+
+  selected_db=$(zenity --list \
+    --title="List of Databases" \
+    --text="Choose a DB to connect to:" \
+    --column="Databases" $database_list)
+
+  if [ $? -eq 1 ]; then
+    DBmenu
+    return
+  fi
+
+  zenity --question --text="Do you want to connect to '$selected_db'.db?"
+  response=$?
+  if [ $response -eq 0 ]; then
+    connect_to_database "$selected_db"
+  else
+    echo "You chose not to connect to any database."
+    DBmenu
+    return
+  fi
+}
+
+function connect_to_database() {
+  local db_name=$1
+
+  cd "Databases/$db_name"
+  echo "You are now connected to the database: $db_name.db"
+  echo "Current directory: $(pwd)"
+}
+
